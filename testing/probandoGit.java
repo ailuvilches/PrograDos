d2909c17c8755cd55bc1c1e7e0024073665f2569
Ailen Vilches <ailenpereiravilches@gmail.com>
2022-08-22 15:26:00 -0300

    Inicializando proyecto

---
+++ testing/probandoGit.java
@@ -0,0 +1,11 @@
+
+public class probandoGit {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hola");
+		System.out.println("hola ailu");
+	}
+
+}
